dbd7ba359d1cef6d626e68acf17ade2c1da50960
Renan <renan.ponick@prof.sc.senac.br>
2026-08-05 22:13:25 -0300

    get

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ import express from 'express' // TYPE MODULE
 // inicializando o express - new 
 const app = express()
 
-// http://localhost:3000/api/somar?num1=30&num2=40
+// Exercicio 1
 app.get("/api/somar", (req, res) => {
     const num1 = Number(req.query.num1)
     const num2 = Number(req.query.num2)
-    res.send({ message: num1 + num2 })
+    // () = 99 % casos é funcao
+    // {} = objeto 
+    res.send({ resultado: num1 + num2 })
 })
+
+// Exercicio 2
+app.get("/api/salario/:valor/:hora", (req, res) => {
+    const valorGanho = Number(req.params.valor)
+    const horasTrabalhadas = Number(req.params.hora)
+
+    const resultado = valorGanho * horasTrabalhadas
+    res.send({ message: resultado })
+})
+
+
+//
+
+
+//
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
