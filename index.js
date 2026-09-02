20a2a94b8bd184d9c10b1b216353d43610ebf97e
Renan <renan.ponick@prof.sc.senac.br>
2026-09-02 18:51:33 -0300

    fim

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ app.use(express.json())
 app.post("/api/somar", (req, res) => {
     const num1 = req.body.num1
     const num2 = req.body.num2
-    res.send({ resultado: num1 + num2 })
+    res.send({ mensagem: num1 + num2 })
 })
 
-app.get("/api/salario/:valor/:hora", (req, res) => {
-    const valorGanho = Number(req.params.valor)
-    const horasTrabalhadas = Number(req.params.hora)
-
-    const resultado = valorGanho * horasTrabalhadas
-    res.send({ message: resultado })
+app.get("/api/div/:num1/:num2", (req, res) => {
+    const num1 = Number(req.params.num1)
+    const num2 = Number(req.params.num2)
+    const resultado = num1 / num2
+    res.send({ mensagem: resultado })
+})
+app.get("/api/mult", (req, res) => {
+    // usar o ? na url
+    const num1 = req.query.num1
+    const num2 = req.query.num2
+    res.send({ message: num1 * num2 })
 })
 
 app.listen(3000, () => {
