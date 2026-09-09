a15b19d3acf4226245baee284c8bf6ccb3a1e1be
Renan <renan.ponick@prof.sc.senac.br>
2026-09-09 19:44:56 -0300

    fim

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# aula-exemplo-backend-jp-2026
-Material das aulas do prof renan
+Crie um repositório novo no GitHub chamado APICalculadora utilizando a arquitetura REST
+Ela deve possuir as 4 operações básicas, assim como potência e raiz (Total: 6 operações);
+Realize também a separação adequada dos arquivos conforme vimos em aula.
+
+Requisitos:
+Possua um método GET que retorne a lista das operações disponíveis;
+Realize as 6 operações matemáticas usando métodos POST;
--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-import express from 'express' 
+import express from 'express'
 
-const app = express()
+const app = express();
 app.use(express.json())
+
+app.get("/api/operacoes", (req, res) => {
+    res.send({ mensagem: "A api pode: Somar, Subtrair, Multiplicar, Div, Potecn, Raizar"})
+})
 
 app.post("/api/somar", (req, res) => {
     const num1 = req.body.num1
     const num2 = req.body.num2
-    res.send({ mensagem: num1 + num2 })
+    const soma = num1 + num2
+    res.send({ soma })
 })
-
-app.get("/api/div/:num1/:num2", (req, res) => {
-    const num1 = Number(req.params.num1)
-    const num2 = Number(req.params.num2)
-    const resultado = num1 / num2
-    res.send({ mensagem: resultado })
+app.post("/api/sub", (req, res) => {
+    const num1 = req.body.num1
+    const num2 = req.body.num2
+    const sub = num1 - num2
+    res.send({ sub })
 })
-app.get("/api/mult", (req, res) => {
-    // usar o ? na url
-    const num1 = req.query.num1
-    const num2 = req.query.num2
-    res.send({ message: num1 * num2 })
+app.post("/api/mult", (req, res) => {
+    const num1 = req.body.num1
+    const num2 = req.body.num2
+    const mult = num1 * num2
+    res.send({ mult })
+})
+app.post("/api/div", (req, res) => {
+    const num1 = req.body.num1
+    const num2 = req.body.num2
+    const div = num1 / num2
+    res.send({ div })
+})
+app.post("/api/pot", (req, res) => {
+    const num1 = req.body.num1
+    const num2 = req.body.num2
+    const pot = num1 ** num2
+    res.send({ pot })
+})
+app.post("/api/raiz", (req, res) => {
+    const num1 = req.body.num1
+    const raiz = num1 ** 0.5
+    res.send({ raiz })
 })
 
 app.listen(3000, () => {
-    console.log("Servidor na porta 3000")
+    console.log("Servidor ouvind na porta 3000")
 })
